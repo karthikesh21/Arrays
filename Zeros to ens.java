a98e7fc30f7ea5c5c9a7ef207933b9514ab3f95f
Soma Karthikesh <karthikes004h@gmail.com>
2025-06-05 01:02:12 +0530

    Create Zeros to ens.java

---
+++ Zeros to ens.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class zerosend {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                System.out.print("0 ");
+            }
+        }
+    }
+}
